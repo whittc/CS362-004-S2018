eee45befb6f5f6d961501f11f682368bfe4a5273
Casey Whitt <whittc@oregonstate.edu>
2018-04-15 09:35:51 -0700

    dominion_helpers.h refactoring update

--- dominion/dominion_helpers.h
+++ dominion/dominion_helpers.h
@@ -9,6 +9,11 @@ int discardCard(int handPos, int currentPlayer, struct gameState *state,
 		int trashFlag);
 int gainCard(int supplyPos, struct gameState *state, int toFlag, int player);
 int getCost(int cardNumber);
+int adventurerEffect(int drawnCard, struct gameState *state, int currentPlayer);
+int smithyEffect(int currentPlayer, struct gameState *state,  int handpos);
+int gardensEffect();
+int villageEffect(int currentPlayer, struct gameState *state, int handpos);
+int sea_hagEffect(int currentPlayer, struct gameState *state);
 int cardEffect(int card, int choice1, int choice2, int choice3, 
 	       struct gameState *state, int handPos, int *bonus);
 
